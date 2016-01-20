03bc56b221979051b80a5d05b6c64f0cba0381e6
Sirojan Tharmakulasingam <sirojan.11@cse.mrt.ac.lk>
2016-01-20 23:49:57 +0530

    Map added

---
+++ app/src/main/java/com/uom/cse/shoppinglist/MapFragment.java
@@ -0,0 +1,55 @@
+package com.uom.cse.shoppinglist;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.uom.cse.shoppinglist.DAO.Item;
+import com.uom.cse.shoppinglist.DAO.ItemsDBHandler;
+
+import java.util.Date;
+
+public class MapFragment extends Fragment {
+
+    private View mapView;
+
+    private GoogleMap mMap;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        mapView = inflater.inflate(R.layout.activity_maps, container, false);
+
+        //setUpMapIfNeeded();
+        return mapView;
+    }
+
+    private void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            mMap = ((SupportMapFragment) getActivity().getSupportFragmentManager().findFragmentById(R.id.map))
+                    .getMap();
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+                setUpMap();
+            }
+        }
+    }
+
+    private void setUpMap() {
+        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+    }
+}
